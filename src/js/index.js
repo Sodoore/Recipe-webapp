63be67902f774e22a1ea6d13eefbfa3850cb916c
Sodoore <104812173+Sodoore@users.noreply.github.com>
2022-08-19 03:45:35 +0800

    search icon gargalaa

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,38 @@
-import axios from 'axios'
-import Search from './model/search'
+import axios from 'axios';
+import Search from './model/search';
+import { elements, renderLoader, clearLoader} from './viev/base';
+import * as searchView from './viev/searchView';
 
-let search = new Search("pasta");
+/**
+ * Web app төлөв
+ * Хайлтын query, үр дүн
+ * Тухайн үзүүлж баыгаа жор
+ * Лайкласан жор
+ * Захиалж байгаа жорын орц
+ */
 
-search.doSearch().then( r => console.log(r) );
+const state = {};
+
+const controlSearch = async () =>{
+    // 1) Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
+    const query = searchView.getInput();
+
+    if(query){
+        // 2) Шинээр хайлтын обьектыг үүсгэж өгнө.
+        state.search = new Search(query);
+        // 3) Хайлт Хийхэд зориулж дэлгэцийг (UI) бэлтгэнэ.
+        searchView.clearSearch();
+        renderLoader(elements.searchResultDiv);
+        // 4) Хайлтыг гүйцэтгэнэ.
+        await state.search.doSearch()
+        // 5) Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+        clearLoader();
+        if(state.search.result === undefined) alert("Хайлт илэрцгүй байна")
+        else searchView.renderRecipes(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+    controlSearch();
+})
